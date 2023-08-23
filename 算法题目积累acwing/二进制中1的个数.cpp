1e073f59a93828264047678e8ce913de4b9ba2e6
Jackson <jacksonxie612@gmail.com>
2023-08-23 22:54:41 +0800

    commit

--- 算法题目积累acwing/二进制中1的个数.cpp
+++ 算法题目积累acwing/二进制中1的个数.cpp
@@ -1,5 +1,33 @@
 /*
 acwing 801.
 
+1. lowbit操作，减去最后一位1
+2. 根据lowbit操作的性质，通过lowbit操作的次数得出有几个1
 */
 
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int lowbit(int x) {
+    return x & -x;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    while (n --) {
+        int x;
+        cin >> x;
+        
+        int res = 0;
+        while (x > 0) { 
+            x -= lowbit(x); //减去带1的位置，直到x被减完
+            res ++; //每减一次代表有一个1，res++
+        }
+        cout << res << ' ';
+    }
+    return 0;
+}
