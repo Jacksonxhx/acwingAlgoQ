81aa47796a60a067676ba73c19eb8faced273b9a
Jackson <jacksonxie612@gmail.com>
2023-08-25 15:12:03 +0800

    update

--- 算法题目积累acwing/最长连续不重复子序列.cpp
+++ 算法题目积累acwing/最长连续不重复子序列.cpp
@@ -13,13 +13,13 @@ int n, a[N], s[N]; //s记录了这个数组中，每个数出现了多少次
 
 int main() {
     scanf("%d", &n);
-    for (int i = 0; i < n; i ++) scanf("%d", &a[i]);
+    for (int i = 0; i < n; i ++) scanf("%d", &a[i]); //读入数组
     
     int res = 0;
     for (int i = 0, j = 0; i < n; i ++) { //i往后走
         s[a[i]] ++; //数组记录每次值出现的次数
     
-        while (s[a[i]] > 1) {
+        while (s[a[i]] > 1) { //如果出现了重复的情况
             s[a[j]] --; //每次j往左走，看j能走多远
             j ++; //如果区间里有一样的，j沿着i的路径一路走过来，确保从j到i之间，所有数就只出现过一次
         }
